6c30ff93e3438b058baa2231723f43502471f223
Dragos Jipa <jipadragos09@gmail.com>
2022-06-06 22:07:20 +0300

    spacing

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 let eligibleParticipantsList = [];
 const participantsList = [
   {
